a12ea65d9e5d69c51c5458742764ee39e9bb60cd
Alejandra Gutierrez <98alejag@gmail.com>
2025-10-24 09:45:55 -0500

    Eliminnación de DTO de la entidad de pacientes

--- src/dto/update-patient.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsBoolean, IsOptional } from 'class-validator';
-import { CreatePatientDTO } from './create-patient.dto';
-
-export class UpdatePatientDTO extends CreatePatientDTO {
-  @IsOptional()
-  @IsBoolean()
-  status?: boolean;
-}

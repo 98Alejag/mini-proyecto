a73bed6c23e7c626ab35f6ccb30497c9ba51e9fa
Alejandra Gutierrez <98alejag@gmail.com>
2025-10-29 13:11:50 -0500

    [DOCS] Documentación con Swagger - Autenticación Full

--- src/dto/Login.dto.ts
+++ src/dto/Login.dto.ts
@@ -1,9 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, MaxLength, MinLength } from "class-validator";
 
 export class LoginDTO {
+    
+    @ApiProperty({example:'example@email.com', description:'Email valido del usuario', required: false})
     @IsEmail()
     email: string;
-
+    
+    @ApiProperty({example:'123456', description:'La contraseña debe tener minimo 6 caracteres y máximo 10', required: false})
     @MinLength(6)
     @MaxLength(10)
     password: string;
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,23 +3,32 @@ import { AuthService } from './auth.service';
 import { LoginDTO } from 'src/dto/Login.dto';
 import { CreateUserDTO } from 'src/dto/create-user.dto';
 import { JwtAuthGuard } from './jwt.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('/api/auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('register')
+    @ApiOperation({summary: 'Registra un usuario'})
+    @ApiResponse({status: 200, description: 'Usuario registrado con exito en la base de datos y nos devuelve el usuario'})
     register(@Body() data: CreateUserDTO) {
         return this.authService.register(data);
     }
 
     @Post('login')
+    @ApiOperation({summary: 'Inicia sesión de un usuario'})
+    @ApiResponse({status: 200, description: 'Usuario logueado con exito y nos devuelve el token de acceso'})
+    @ApiResponse({status: 401, description: 'Credenciales invalidas'})
     login(@Body() data: LoginDTO) {
         return this.authService.login(data);
     }
 
     @UseGuards(JwtAuthGuard)
     @Get('profile')
+    @ApiBearerAuth()
+    @ApiOperation({summary: 'Obtiene el perfil del usuario que esta loggeado'})
+    @ApiResponse({status: 200, description: 'Información del usuario'})
     getProfile(@Request() req) {
         return req.user;
     }

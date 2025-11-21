8914fe58b6118d64e9885e244d6efccdac2af069
Alejandra Gutierrez <98alejag@gmail.com>
2025-11-21 15:58:27 -0500

    [fix] Migraciones

--- src/migrations/1763757813107-FirstMigration.ts
+++ src/migrations/1763757813107-FirstMigration.ts
@@ -1,12 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitMigration1761274078141 implements MigrationInterface {
-    name = 'InitMigration1761274078141'
+export class FirstMigration1763757813107 implements MigrationInterface {
+    name = 'FirstMigration1763757813107'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`clinical_history\` (\`id\` int NOT NULL AUTO_INCREMENT, \`date\` varchar(255) NOT NULL, \`reasonForVisit\` varchar(255) NOT NULL, \`diagnosis\` varchar(255) NOT NULL, \`proposedTreatment\` varchar(255) NULL, \`patientId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`phone\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`address\` varchar(255) NULL, \`age\` int NULL, \`status\` tinyint NOT NULL DEFAULT 1, \`role\` varchar(255) NOT NULL DEFAULT 'patient', UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`treatment\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`description\` text NULL, \`price\` decimal(10,2) NOT NULL, \`status\` tinyint NOT NULL DEFAULT 1, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`appointment\` (\`id\` int NOT NULL AUTO_INCREMENT, \`datehour\` timestamp NOT NULL, \`durationMinutes\` int NOT NULL DEFAULT '30', \`status\` varchar(255) NOT NULL DEFAULT 'pendiente', \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`doctorId\` int NULL, \`patientId\` int NULL, \`treatmentId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`clinical_history\` ADD CONSTRAINT \`FK_6146817e142e2317a2a123ce71b\` FOREIGN KEY (\`patientId\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`appointment\` ADD CONSTRAINT \`FK_514bcc3fb1b8140f85bf1cde6e2\` FOREIGN KEY (\`doctorId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`appointment\` ADD CONSTRAINT \`FK_5ce4c3130796367c93cd817948e\` FOREIGN KEY (\`patientId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`appointment\` ADD CONSTRAINT \`FK_91bba5a127e0958278e7e0cb577\` FOREIGN KEY (\`treatmentId\`) REFERENCES \`treatment\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -16,10 +18,12 @@ export class InitMigration1761274078141 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`appointment\` DROP FOREIGN KEY \`FK_91bba5a127e0958278e7e0cb577\``);
         await queryRunner.query(`ALTER TABLE \`appointment\` DROP FOREIGN KEY \`FK_5ce4c3130796367c93cd817948e\``);
         await queryRunner.query(`ALTER TABLE \`appointment\` DROP FOREIGN KEY \`FK_514bcc3fb1b8140f85bf1cde6e2\``);
+        await queryRunner.query(`ALTER TABLE \`clinical_history\` DROP FOREIGN KEY \`FK_6146817e142e2317a2a123ce71b\``);
         await queryRunner.query(`DROP TABLE \`appointment\``);
         await queryRunner.query(`DROP TABLE \`treatment\``);
         await queryRunner.query(`DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``);
         await queryRunner.query(`DROP TABLE \`user\``);
+        await queryRunner.query(`DROP TABLE \`clinical_history\``);
     }
 
 }
---
+++ src/migrations/1763757908792-SeedAdmin.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+const ADMIN_EMAIL = 'admin@routify.com';
+const ADMIN_HASH = '$2b$10$46LRH3Z7D.j1PruDIgnYvuBdnX7rVAIlobTd2KhJUrG3Q00qC5G3q'; // ejemplo para 'admin123'
+const ADMIN_NAME = 'Administrador';
+const ADMIN_PHONE = '3000000000'; // Cambia por un número real si lo deseas
+const ADMIN_ADDRESS = 'Dirección Admin'; // Opcional, puedes dejarlo vacío o con un valor genérico
+
+
+export class SeedAdmin1763757908792 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+      INSERT INTO users 
+        (email, password, name, phone, address, age, role, status)
+      SELECT 
+        '${ADMIN_EMAIL}', 
+        '${ADMIN_HASH}', 
+        '${ADMIN_NAME}', 
+        '${ADMIN_PHONE}', 
+        '${ADMIN_ADDRESS}', 
+        NULL, -- Puedes asignar una edad si lo deseas
+        'admin', 
+        true
+      WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = '${ADMIN_EMAIL}');
+    `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+      DELETE FROM users WHERE email = '${ADMIN_EMAIL}';
+    `);
+    }
+
+}
